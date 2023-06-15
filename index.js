41ba8fd27e712ce08f7d1ce909a2f618f2f0e74a
NASH <nashon.mwenda@student.moringaschool.com>
2023-06-15 15:31:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, $Grace Hopper!'
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, $GRACE HOPPER!'
+const shortGreeting = "Welcome, $G!"
+
+
+
 // Write your code in this file!
